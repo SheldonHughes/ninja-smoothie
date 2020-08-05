105d98116f7cfb904e808cd15530819ca0dafc57
SheldonHughes <sheldondhughes@gmail.com>
2020-08-04 21:22:02 -0500

    Lesson 2.5

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended indigo darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title"></span>
+                </router-link>
+                <a href="" class="btn-floating btn-large halfway-fab pink"></a>
+            </div>
+        </nav>
+    </div>
+</template>
