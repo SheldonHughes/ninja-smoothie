6639879d924c80ff78977fad5debf0268ef6d652
SheldonHughes <sheldondhughes@gmail.com>
2020-08-04 21:13:59 -0500

    lesson 2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card-panel teal lighten-2">
+    <span class="blue-text text-darken-1">This is an about page</span>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,12 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Index from '@/components/Index.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
